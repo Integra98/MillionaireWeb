d309e7407f025cb003f6638944701076382915bb
Helen <helena.dyu251198@gmail.com>
2020-06-15 17:58:51 +0600

    events styles

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,18 +12,77 @@ import { Subscription } from 'rxjs';
 export class HomeComponent implements OnInit {
   language: string;
   subscription: Subscription;
-
+  cards = [
+    {
+      title: 'Card Title 1',
+      description: 'This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
+      buttonText: 'Button',
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg',
+      isShowDesc: true
+    },
+    {
+      title: 'Card Title 2',
+      description: 'This card has supporting text below as a natural lead-in to additional content.',
+      buttonText: 'Button',
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg',
+      isShowDesc: false
+    },
+    {
+      title: 'Card Title 3',
+      description: 'This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action. This text is much longer so that you can see a significant difference between the text in  previous tabs.',
+      buttonText: 'Button',
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg',
+      isShowDesc: false
+    },
+    {
+      title: 'Card Title 4',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      buttonText: 'Button',
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg',
+      isShowDesc: false
+    },
+    {
+      title: 'Card Title 5',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      buttonText: 'Button',
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg',
+      isShowDesc: false
+    },
+    {
+      title: 'Card Title 6',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      buttonText: 'Button',
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg',
+      isShowDesc: false
+    }
+  ];
+  slides: any = [[]];
+  
   constructor(private service: MainService) {
     this.subscription = this.service.getSelectedLanguage().subscribe(res => {
-        this.language = res;
+      this.language = res;
     });
   }
-
+  
   ngOnInit() {
+    this.slides = this.chunk(this.cards, 3);
+    console.log(this.slides);
   }
+      chunk(arr: any, chunkSize:any) {
+        let R = [];
+        for (let i = 0, len = arr.length; i < len; i += chunkSize) {
+          R.push(arr.slice(i, i + chunkSize));
+        }
+        return R;
+      }
 
   changeLanguage(lan: string) {
-    this.service.setSelectedLanguage(lan);
+    // this.service.setSelectedLanguage(lan);
+  }
+
+  showDescription(flag, index){
+    this.cards[index].isShowDesc = flag;
+    console.log(this.cards[index]);
   }
 
   // tslint:disable-next-line:use-lifecycle-interface
