889cef0c557397a53cad692cd05bb23ed2e4f4f7
Helen <helena.dyu251198@gmail.com>
2020-06-16 18:00:29 +0600

    none

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { trigger, transition, group, query, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'millionaireWeb';
@@ -11,4 +13,5 @@ export class AppComponent {
   changeLanguage(lan: string) {
     // this.service.setSelectedLanguage(lan);
   }
+
 }
